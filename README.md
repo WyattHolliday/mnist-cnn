c6cf2e958b4acd3e76d7aa89d961b588fc4bc790
WyattHolliday <wph03@icloud.com>
2024-07-14 17:41:56 -0700

    Added readme and custom image classification

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mnist-cnn
+## Overview
+* Trains a convolutional neural network on the mnist data to classify hand drawn images of single digit numbers
+* Install imported libraries and run main.py to train and then test the model
+* By defualt runs 10 epochs with a learning rate of 0.001 and batch size of 100, all hyperparameter changes will need to be made manually
+## Classifying Custom Imanges
+To label new images in the custom_images folder after training and testing, follow these steps for the best results:
+1. Open the Paint application and resize the canvas to 28x28 pixels.
+2. Fill all pixels with black and then create your single digit with a small white brush.
+3. Save and add your image to the custom_images folder.
+4. On the next run, it should display the name of the image and its attempted classification.
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from torchvision import datasets
+from torchvision import transforms
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import Dataset
+from PIL import Image
+from pathlib import Path
+import os
 
 train_data = datasets.MNIST(root='data', train=True, download=True, transform=ToTensor())
 test_data = datasets.MNIST(root='data', train=False, download=True, transform=ToTensor())
@@ -18,8 +23,9 @@ class ConvolutionalNeuralNetwork(nn.Module):
     def __init__(self):
         super(ConvolutionalNeuralNetwork, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # conv output width = square image width - kernel size + 1 (/ 2 for pooling)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5) # 1x28x28 -> 10x12x12
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5) # 10x12x12 -> 20x4x4 = 320
         self.conv2_drop = nn.Dropout2d() # randomly zeroes some elements during training
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, 10)
@@ -48,7 +54,7 @@ def train(epoch):
         loss = loss_fn(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % 25 == 0:
+        if batch_idx % 50 == 0:
             print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(loaders["train"].dataset)} ({100. * batch_idx / len(loaders["train"]):.0f}%)]\tLoss: {loss.item()}')
 
 def test():
@@ -66,11 +72,42 @@ def test():
     test_loss /= len(loaders['test'].dataset)
     print(f'\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(loaders["test"].dataset)} ({100. * correct / len(loaders["test"].dataset):.0f}%)')
 
+class CustomImageDataset(Dataset):
+    def __init__(self, image_folder):
+        self.image_folder = image_folder
+        self.transform = transforms.Compose([
+            transforms.Resize((28, 28)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+        self.image_files = [file for file in os.listdir(image_folder) if os.path.isfile(os.path.join(image_folder, file))]
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.image_folder, self.image_files[idx])
+        image = Image.open(img_name).convert('L')  # MNIST images are grayscale
+        image = self.transform(image)
+        return image, self.image_files[idx]
+
+def classify_custom_images():
+    dataset = CustomImageDataset(str(Path(__file__).parent.absolute()) + '\\custom_images')
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+    with torch.no_grad():
+        for images, filenames in dataloader:
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            for filename, prediction in zip(filenames, predicted):
+                print(f'Image: {filename}, Predicted Label: {prediction.item()}')
+
 
 if __name__ == '__main__':
     for epoch in range(1, 11):
         train(epoch)
         test()
+    classify_custom_images()
 
     # model_path = 'models/mnist_cnn.pth'
     # torch.save(model.state_dict(), str(Path(__file__).parent.absolute()) + model_path)
